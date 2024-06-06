cd8f97fa17d06fadf3ad5cfb51e0714c4ae5e196
Eduardo <calle13.201@gmail.com>
2024-06-05 20:56:42 -0600

    Añadir datatables por # y añadir a tabla rotos campos de buscar, paginado

--- static/js/datatables/datatable_init.js
+++ static/js/datatables/datatable_init.js
@@ -1,24 +1,46 @@
 $(document).ready(function() {
-        $('#rotos').DataTable({
-            ajax: {
-                url: '/producto_no_conforme/lecherias/data',
-                dataSrc: ''
+    $('#rotos').DataTable({
+        ajax: {
+            url: '/producto_no_conforme/lecherias/data',
+            dataSrc: ''
+        },
+        
+        columns: [
+            { data: 'numero' },
+            { data: 'nombre' },
+            { data: 'responsable' },
+            { data: 'telefono' },
+            { data: 'direccion' },
+            { data: 'municipio' },
+            { data: 'numero_ruta' },
+            { data: 'opciones'},
+        ],
+
+        pageLength: 5,
+        lengthMenu: [[5, 10, 25, 50, -1], [5, 10, 25, 50, "Todo"]],
+        language: {
+            lengthMenu: "Mostrar _MENU_ entradas",
+            zeroRecords: "No se encontraron resultados",
+            info: "Mostrando entradas _START_ a _END_ de _TOTAL_",
+            infoEmpty: "Mostrando entradas 0 a 0 de 0",
+            infoFiltered: "(filtrado de _MAX_ entradas totales)",
+            search: "Buscar:",
+            paginate: {
+                first: "Primero",
+                last: "Último",
+                next: "Siguiente",
+                previous: "Anterior"
             },
-            
-            columns: [
-                { data: 'id' },
-                { data: 'numero' },
-                { data: 'nombre' },
-                { data: 'responsable' },
-                { data: 'telefono' },
-                { data: 'direccion' },
-                { data: 'poblacion_id' },
-                { data: 'ruta_id' },
-                { data: 'opciones'},
-                // ... más campos aquí ...
-            ]
-        });
+        },
+        columnDefs: [
+            {
+                targets: -1,
+                defaultContent: "<div style='display: flex; justify-content: space-between;'><button class='btn btn-secondary' style='font-size: 14px; width:75px;'><i class='fas fa-pencil'></i></button><button class='btn btn-danger' style='font-size: 14px; width: 75px;' ><i class='fas fa-trash'></i></button></div>",                 
+                orderable: false
+            }
+        ]
     });
+});
 
 
     $(document).ready(function() {
