afb8e30a3432835b8a06e540b0037da11489ce58
Eduardo <calle13.201@gmail.com>
2024-06-05 20:55:55 -0600

    Modificacion vista Lecheria DataView

--- producto_no_conforme/views.py
+++ producto_no_conforme/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views import View
 from .models import Lecheria
+from django.db.models import F
 
 class LecheriaListView(View):
     def get(self, request, *args, **kwargs):
@@ -9,5 +10,10 @@ class LecheriaListView(View):
 
 class LecheriaDataView(View):
     def get(self, request, *args, **kwargs):
-        lecherias_list = list(Lecheria.objects.values())
+        lecherias = Lecheria.objects.annotate(
+            municipio=F('poblacion__municipio'),
+            numero_ruta=F('ruta__numero')
+        ).values(  'numero', 'nombre', 'responsable', 'municipio', 'telefono', 'direccion','numero_ruta')
+        
+        lecherias_list = list(lecherias)
         return JsonResponse(lecherias_list, safe=False)
