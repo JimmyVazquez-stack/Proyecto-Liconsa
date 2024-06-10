2920489f5fdcd102b858ef4728f9373681b2ab88
Eduardo <calle13.201@gmail.com>
2024-06-10 15:46:22 -0600

    Vista de login

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class index(LoginRequiredMixin, TemplateView):
+    template_name = 'iniciar_sesion.html'
+    login_url = reverse_lazy('laboratorio_control_calidad:index') 
