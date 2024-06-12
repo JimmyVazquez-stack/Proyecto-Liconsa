504bd351cb43bfd98df6ca2aa55c932140347ebd
Jucamora <166667024+Jucamora@users.noreply.github.com>
2024-06-12 14:13:07 -0600

    Agregar vista para formulario

--- laboratorio_control_calidad/views.py
+++ laboratorio_control_calidad/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from laboratorio_control_calidad.forms import TablaR49Form
+from laboratorio_control_calidad.models import TablaR49
+from .forms import TablaR49Form
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 class index(TemplateView):
@@ -35,3 +39,35 @@ class calibracion_verificacion_equipo(TemplateView):
 class verificacion_documentos(TemplateView):
     template_name = 'verificacion_documentos.html'
     
+
+class tabla_r49(TemplateView): 
+    template_name = 'tabla_r49.html' 
+
+
+class crear_registror49(CreateView):
+    template_name = 'crear_registror49.html'
+    form_class = TablaR49
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+
+    # 
+    # get(self, request, *args, **kwargs): 
+
+    #     form = TablaR49Form(request.POST) 
+    #     if form.is_valid():
+    #         num_maquina = formulario.cleaned_data['num_maquina']
+    #         num_datos = formulario.cleaned_data['num_datos']
+    #         promedio = formulario.cleaned_data['promedio']
+    #         desv_std = formulario.cleaned_data['desv_std']
+    #         maximo = formulario.cleaned_data['maximo']
+    #         minimo = formulario.cleaned_data['minimo']
+    #         form.save() 
+    #     return render(request, 'tabla_r49.html', {'form': form})
+    
+        #return self.render_to_response({'form': form}) 
+    
+
+
+     
