d7ba202264af6a8451e473a471bae5418816596a
Eduardo <calle13.201@gmail.com>
2024-06-11 19:25:53 -0600

    Registrar modelo usuario al admin

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User, Group
 from .forms import UserChangeForm, UserCreationForm
+from .models import Usuario
+
+admin.site.register(Usuario)
+
 '''
 class UserAdmin(BaseUserAdmin):
     form = UserChangeForm
