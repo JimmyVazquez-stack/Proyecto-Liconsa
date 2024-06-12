0672169ecbe8832e6923fe8f018642b0dec6b1ae
Jucamora <166667024+Jucamora@users.noreply.github.com>
2024-06-12 14:08:00 -0600

    Creacion y Registro modelo TablaR49

--- laboratorio_control_calidad/models.py
+++ laboratorio_control_calidad/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TablaR49(models.Model): 
+    num_maquina = models.CharField(max_length=2) 
+    num_datos = models.IntegerField() 
+    promedio = models.IntegerField() 
+    desv_std = models.IntegerField() 
+    maximo = models.IntegerField() 
+    minimo = models.IntegerField() 
+
+ 
+    def __str__(self): 
+     return '%s - %s' % (self.num_maquina, self.promedio) 
