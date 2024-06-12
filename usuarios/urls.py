6f30e2ca3a9ed0286ff3e69de9b650a1eab154c5
Eduardo <calle13.201@gmail.com>
2024-06-11 19:26:18 -0600

    Rutas de login

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from .views import LoginView
 
 app_name = 'usuarios'
 
 urlpatterns = [
-    path('', auth_views.LoginView.as_view(template_name='iniciar_sesion.html'), name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path ('logout/', auth_views.LogoutView.as_view(), name='logout'),
     ]
