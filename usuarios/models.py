a1e26bf859d7c9d3c3d60df439fe3683d8da4f96
Eduardo <calle13.201@gmail.com>
2024-06-11 19:26:07 -0600

    Modelo usuario

--- usuarios/models.py
+++ usuarios/models.py
@@ -4,6 +4,7 @@ from catalogos.models import Area
 
 
 class Usuario(AbstractUser):
+    telefono = models.CharField(max_length=10, null=True)
     area=models.ForeignKey(Area, on_delete=models.CASCADE, null=True)
     
     def __str__(self): 
