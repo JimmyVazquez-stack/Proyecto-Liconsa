26256f4d257ecfca6520981c81514acf2ea462fa
Eduardo <calle13.201@gmail.com>
2024-06-11 19:21:48 -0600

    Registrar modelo Area a modulo admin

--- catalogos/admin.py
+++ catalogos/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Lecheria, Ruta, Poblacion, Rotos
+from .models import Lecheria, Ruta, Poblacion, Rotos, Area
 
 # Register your models here.
 admin.site.register(Lecheria)
 admin.site.register(Ruta)
 admin.site.register(Poblacion)
 admin.site.register(Rotos)
+admin.site.register(Area)
