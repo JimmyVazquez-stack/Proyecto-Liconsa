5dad3145eb0a8e7e0a5bba8f3dcf634ed4182260
Eduardo <calle13.201@gmail.com>
2024-06-12 20:23:10 -0600

    Ruta a perfil de usuario

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -8,4 +8,5 @@ app_name = 'usuarios'
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path ('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('perfil/', views.PerfilView.as_view(), name='perfil'),
     ]
