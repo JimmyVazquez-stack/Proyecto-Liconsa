d4e1a9c8fed245c769b7a670a8599e5aa7bf52e5
Eduardo <calle13.201@gmail.com>
2024-06-12 20:23:35 -0600

    Vista a perfil de usuario, ListView

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,12 +3,20 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView
-
-
+from django.views.generic import ListView
+from .models import Usuario
 
 
 class LoginView(LoginView):
     template_name = 'registration/login.html'
     success_url = reverse_lazy('laboratorio_control_calidad:index')
+
+
+class PerfilView(LoginRequiredMixin, ListView):
+    template_name = 'perfil.html'
+    model = Usuario
+    
+    def get_queryset(self):
+        return Usuario.objects.filter(username=self.request.user)
     
     
