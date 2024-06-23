0b42a18cadb62b72df5f04f39f308436877bae39
Eduardo <calle13.201@gmail.com>
2024-06-22 19:58:08 -0600

    Ruta para la creacion de usuario

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path ('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('perfil/', views.PerfilView.as_view(), name='perfil'),
-    ]
+    path('crear_usuario/', views.CrearUsuarioView.as_view(), name='crear_usuario'),
+]
