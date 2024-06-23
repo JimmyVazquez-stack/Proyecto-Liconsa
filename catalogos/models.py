4044216a3fa3f7b796eb092b992cf33951ef4d53
Eduardo <calle13.201@gmail.com>
2024-06-22 20:40:35 -0600

    Modelos nuevos para reportes

--- catalogos/models.py
+++ catalogos/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-    
+from django.core.exceptions import ValidationError 
+'''
+Importamos ValidationError para poder validar los datos que se ingresan en los campos de los modelos.
+'''
 class Ruta(models.Model):
     nombre = models.CharField(max_length=50)
     numero = models.IntegerField()
@@ -53,7 +56,8 @@ class Rotos(models.Model):
     class Meta:
         verbose_name_plural = "Roto"
         verbose_name_plural = "Rotos"
-        
+
+#Modelo usado en usuarios
 class Area(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=50)
@@ -63,3 +67,93 @@ class Area(models.Model):
     
     class Meta:
         verbose_name_plural = "Areas"
+        
+
+
+#Modelos generales y reportes
+class Planta(models.Model):
+    nombre = models.CharField(max_length=100)
+    ubicacion = models.CharField(max_length=100)
+    correo = models.EmailField(max_length=100)
+    contacto = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=100)
+    
+
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        verbose_name_plural = "Planta"
+        verbose_name_plural = "Plantas"
+
+class Proveedor(models.Model):
+    nombre = models.CharField(max_length=100)
+    contacto = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=100)
+    correo = models.EmailField(max_length=100)
+    planta = models.ForeignKey(Planta, on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name_plural = "Proveedor"
+        verbose_name_plural = "Proveedores"
+class Maquina(models.Model):
+    nombre = models.CharField(max_length=100)
+    planta = models.ForeignKey(Planta, on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        verbose_name_plural = "Maquina"
+        verbose_name_plural = "Maquinas"
+
+class Cabezal(models.Model):
+    codigo = models.CharField(max_length=100)
+    maquina = models.ForeignKey(Maquina, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.codigo
+    
+    class Meta:
+        verbose_name_plural = "Cabezal"
+        verbose_name_plural = "Cabezales"
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name_plural = "Producto"
+        verbose_name_plural = "Productos"
+class Turno(models.Model):
+    descripcion = models.CharField(max_length=100)
+    hora_inicio = models.TimeField()
+    hora_fin = models.TimeField()
+
+    def __str__(self):
+        return self.descripcion
+    
+    class Meta:
+        verbose_name_plural = "Turno"
+        verbose_name_plural = "Turnos"
+
+class Silo(models.Model):
+    numero = models.IntegerField()
+    capacidad = models.DecimalField(max_digits=10, decimal_places=2)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    planta = models.ForeignKey(Planta, on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return f"Silo {self.numero}"
+    
+    class Meta:
+        verbose_name_plural = "Silo"
+        verbose_name_plural = "Silos"
