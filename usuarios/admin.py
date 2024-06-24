5239863596bc73d44ebce2eb7d74e6154c03ab9c
Eduardo <calle13.201@gmail.com>
2024-06-24 13:57:24 -0600

    Eliminar permisos individuales para usuarios registrados, manejar permisos por grupos

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -8,14 +8,14 @@ class UsuarioAdmin(UserAdmin):
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         ('Información personal', {'fields': ('first_name', 'last_name', 'email', 'area', 'telefono')}),
-        ('Permisos', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Permisos', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups')}),
         ('Fechas importantes', {'fields': ('last_login', 'date_joined')}),
     )
 
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'password1', 'password2', 'first_name', 'last_name', 'email', 'area', 'telefono', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+            'fields': ('username', 'password1', 'password2', 'first_name', 'last_name', 'email', 'area', 'telefono', 'is_active', 'is_staff', 'is_superuser', 'groups'),
         }),
     )
     list_display = ('username', 'first_name', 'last_name', 'email', 'telefono', 'is_staff', 'area')
