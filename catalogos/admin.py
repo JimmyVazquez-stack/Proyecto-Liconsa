3227f19f89fd2ebcf13d228417cb90c4668b5b2a
Eduardo <calle13.201@gmail.com>
2024-06-24 13:15:41 -0600

    Registrar TipoProducto

--- catalogos/admin.py
+++ catalogos/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Lecheria, Ruta, Poblacion, Rotos, Area, Producto, Planta, Proveedor, Maquina, Cabezal, Turno, Silo
+from .models import Lecheria, Ruta, Poblacion, Rotos, Area, Producto, Planta, Proveedor, Maquina, Cabezal, Turno, Silo, TipoProducto
 
 # Register your models here.
 admin.site.register(Lecheria)
@@ -14,3 +14,4 @@ admin.site.register(Maquina)
 admin.site.register(Cabezal)
 admin.site.register(Turno)
 admin.site.register(Silo)
+admin.site.register(TipoProducto)
