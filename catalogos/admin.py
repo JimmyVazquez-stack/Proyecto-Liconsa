3889ddc16ae13c36ea9936e395e0cd68c15e9fd7
Jucamora <166667024+Jucamora@users.noreply.github.com>
2024-06-26 15:04:46 -0600

    Se agregaron los modelos de TablaR49, Densidad, Peso vacio, peso bruto al admin de catalogos.

--- catalogos/admin.py
+++ catalogos/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Lecheria, Ruta, Poblacion, Rotos
+from laboratorio_control_calidad.models import TablaR49, Densidadpt, Pesoenvvacio, Pesobruto #Pesoneto
 
 # Register your models here.
 admin.site.register(Lecheria)
 admin.site.register(Ruta)
 admin.site.register(Poblacion)
 admin.site.register(Rotos)
+
+#Modelos FormatoR49
+admin.site.register(TablaR49)
+admin.site.register(Densidadpt)
+admin.site.register(Pesoenvvacio)
+admin.site.register(Pesobruto)
+# admin.site.register(Pesoneto)
