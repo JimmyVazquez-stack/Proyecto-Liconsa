6ec5f8d432ae37faf646d89cf05021c353fcd536
Jucamora <166667024+Jucamora@users.noreply.github.com>
2024-06-26 15:02:16 -0600

    Agregar los forms de densidad, pesoenvvacio, peso bruto.

--- laboratorio_control_calidad/forms.py
+++ laboratorio_control_calidad/forms.py
@@ -1,7 +1,50 @@
 from django import forms 
-from .models import TablaR49 
+from .models import TablaR49, Densidadpt, Pesoenvvacio, Pesobruto #Pesoneto
 
-class TablaR49Form(forms.ModelForm): 
-    class Meta: 
-        model = TablaR49 
-        fields = ['num_maquina', 'num_datos', 'promedio','desv_std','maximo','minimo'] 
+
+
+class TablaR49Form(forms.ModelForm):
+    class Meta:
+        model = TablaR49
+        fields = '__all__'
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({'class' : 'form-control'})
+
+
+class DensidadptForm(forms.ModelForm):
+    class Meta:
+        model = Densidadpt
+        fields = '__all__'
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({'class' : 'form-control'})
+
+class PesoenvvacioForm(forms.ModelForm):
+    class Meta:
+        model = Pesoenvvacio
+        fields = '__all__'
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({'class' : 'form-control'})
+
+class PesobrutoForm(forms.ModelForm):
+    class Meta:
+        model = Pesobruto
+        fields = '__all__'
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({'class' : 'form-control'})
+
