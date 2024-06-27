9d3c8da8721461cd9f70771b8f89aa62bae29383
Eduardo <calle13.201@gmail.com>
2024-06-27 12:17:41 -0600

    Crear vista a listar usuarios, list view, LoginRequiredMixin a todas las vistas, uso de login url reverse lazy

--- usuarios/views.py
+++ usuarios/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.views import LoginView
 from django.views.generic import ListView
 from .models import Usuario
 from django.shortcuts import get_object_or_404
-from django.views.generic import DetailView, CreateView
+from django.views.generic import DetailView, CreateView, ListView
 from .forms import UserCreationForm
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
@@ -22,18 +22,20 @@ class LoginView(LoginView):
 class PerfilView(LoginRequiredMixin, DetailView):
     template_name = 'perfil.html'
     model = Usuario
-
+    login_url = reverse_lazy('usuarios:login')
+    
     def get_object(self):
         return get_object_or_404(Usuario, username=self.request.user.username)
     
     
-class CrearUsuarioView(CreateView, PermissionRequiredMixin):
+class CrearUsuarioView(LoginRequiredMixin,CreateView, PermissionRequiredMixin):
     form_class = UserCreationForm
     model = Usuario
     permission_required = 'usuarios.add_usuario'
     template_name = 'crear_usuario.html'
     success_url = reverse_lazy('usuarios:crear_usuario')
     success_message = 'Usuario creado exitosamente'
+    login_url  = reverse_lazy('usuarios:login')
     
     def form_valid(self, form):
         username = form.cleaned_data['username']
@@ -44,3 +46,15 @@ class CrearUsuarioView(CreateView, PermissionRequiredMixin):
             response = super().form_valid(form)
             messages.success(self.request, self.success_message)
             return response
+
+
+class ListarUsuariosView(LoginRequiredMixin,ListView, PermissionRequiredMixin):
+    model = Usuario
+    template_name = 'listar_usuarios.html'
+    permission_required = 'usuarios.view_usuario'
+    login_url = reverse_lazy('usuarios:login')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['total_usuarios'] = self.model.objects.count()
+        return context
