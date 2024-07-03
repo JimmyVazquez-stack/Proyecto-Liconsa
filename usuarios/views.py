c69743d629150dc363ab19fc7cfd50fceb285f64
Eduardo <calle13.201@gmail.com>
2024-07-03 14:48:25 -0600

    vista de eliminacion de usuario en datatable usaurios

--- usuarios/views.py
+++ usuarios/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.views import LoginView
 from django.views.generic import ListView
 from .models import Usuario
 from django.shortcuts import get_object_or_404
-from django.views.generic import DetailView, CreateView, ListView, View, UpdateView
+from django.views.generic import DetailView, CreateView, ListView, View, UpdateView, DeleteView
 from .forms import UserCreationForm
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
@@ -16,7 +16,8 @@ from django.http import JsonResponse
 from django.http import HttpResponseForbidden
 from django.db.models import F
 from django.forms.models import model_to_dict
-
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
 
 class LoginView(LoginView):
@@ -51,35 +52,50 @@ class CrearUsuarioView(LoginRequiredMixin, CreateView, PermissionRequiredMixin):
             response = super().form_valid(form)
             messages.success(self.request, self.success_message)
             return response
-        
 
-        
+
+# Deshabilitar la protección CSRF
+@method_decorator(csrf_exempt, name='dispatch')
+class EliminarUsuarioView(View):
+    def delete(self, request, user_id):
+        try:
+            usuario = Usuario.objects.get(id=user_id)
+            usuario.delete()
+            return JsonResponse({'message': 'Usuario eliminado exitosamente'})
+        except Usuario.DoesNotExist:
+            return JsonResponse({'error': 'El usuario no existe'}, status=404)
+
 class ListarUsuariosView(LoginRequiredMixin, ListView, PermissionRequiredMixin):
     model = Usuario
     template_name = 'listar_usuarios.html'
     permission_required = 'usuarios.view_usuario'
     login_url = reverse_lazy('usuarios:login')
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # Excluir usuarios staff al contar
-        context['total_usuarios'] = self.model.objects.exclude(is_staff=True).count()
+        context['total_usuarios'] = self.model.objects.exclude(
+            is_staff=True).count()
         return context
-        
+
+
 class UsuariosDataView(LoginRequiredMixin, View, PermissionRequiredMixin):
     model = Usuario
     login_url = reverse_lazy('usuarios:login')
 
     def get(self, request, *args, **kwargs):
         # Filtrar para excluir usuarios que son staff
-        usuarios_queryset = self.model.objects.filter(is_staff=False).select_related('area').prefetch_related('groups')
+        usuarios_queryset = self.model.objects.filter(
+            is_staff=False).select_related('area').prefetch_related('groups')
         usuarios_list = []
 
         for usuario in usuarios_queryset:
             # Convertir el modelo Usuario a un diccionario
-            usuario_dict = model_to_dict(usuario, fields=[field.name for field in usuario._meta.fields if field.name != 'password'])
+            usuario_dict = model_to_dict(usuario, fields=[
+                                         field.name for field in usuario._meta.fields if field.name != 'password'])
             # Personalizar el diccionario para incluir el campo 'grupo'
-            usuario_dict['grupo'] = ', '.join([group.name for group in usuario.groups.all()])
+            usuario_dict['grupo'] = ', '.join(
+                [group.name for group in usuario.groups.all()])
             # Añadir el nombre del área al diccionario, asegurándose de que el área existe
             usuario_dict['area'] = usuario.area.nombre if usuario.area else None
             usuarios_list.append(usuario_dict)
