617fe1d413be9493fb82f2fa2f21f712ea487dab
Jucamora <166667024+Jucamora@users.noreply.github.com>
2024-07-05 14:47:55 -0600

    Importar modelo analista, cambiar campo fecha para mostrar la hora en encabezado, agregar not null a cmpos foreing key

--- laboratorio_control_calidad/models.py
+++ laboratorio_control_calidad/models.py
@@ -1,30 +1,18 @@
 from django.db import models
 from django.utils import timezone
-from catalogos.models import Cabezal, Planta, Turno, Producto, Proveedor, Silo, Maquina, Area
+from catalogos.models import Cabezal, Planta, Turno, Producto, Proveedor, Silo, Maquina, Area, Analista
 
 # Create your models here.
 class EncabTablaR49(models.Model):
-    fecha = models.DateField(default=timezone.now)
+    fecha = models.DateTimeField(default=timezone.now)
     observaciones = models.CharField(verbose_name="Observaciones",max_length=512, blank=True) 
 
     def __str__(self):
        return f"ID: {self.id}, fecha: {self.fecha}"
 
 
-
-# class TablaR49(models.Model):
-#     numMaquina = models.CharField(max_length=2)
-#     numDatos = models.IntegerField()
-#     promedio = models.IntegerField()
-#     desvStd = models.IntegerField()
-#     maximo = models.IntegerField()
-#     minimo = models.IntegerField()
-
-#     def __str__(self):
-#         return '%s - %s' % (self.numMaquina, self.promedio)
     
 class Densidadpt(models.Model):
-    #NO_SILO = [(1, '1'),(2, '2'),(3, '3'),(4, '4'),(5, '5'),(6, '6'),(7, '7'),(8, '8'),]
     fechaHora = models.DateTimeField (default=timezone.now,verbose_name="Hora")
     cabezal = models.ForeignKey(Cabezal, on_delete=models.CASCADE)
     planta = models.ForeignKey(Planta, on_delete=models.CASCADE)
@@ -60,13 +48,13 @@ class Pesoenvvacio(models.Model):
     
 class Pesobruto(models.Model):
     fechaHora = models.DateTimeField(default=timezone.now,verbose_name="Hora")
-    cabezal = models.ForeignKey(Cabezal, on_delete=models.CASCADE)
-    maquina = models.ForeignKey(Maquina, on_delete=models.CASCADE) 
-    planta = models.ForeignKey(Planta, on_delete=models.CASCADE)
+    cabezal = models.ForeignKey(Cabezal, on_delete=models.CASCADE, null=True)
+    maquina = models.ForeignKey(Maquina, on_delete=models.CASCADE, null=True) 
+    planta = models.ForeignKey(Planta, on_delete=models.CASCADE, null=True)
     producto = models.ForeignKey(Producto,on_delete=models.CASCADE, null=True)
-    analista = models.ForeignKey(Area, on_delete=models.CASCADE)
+    analista = models.ForeignKey(Analista, on_delete=models.CASCADE, null=True)
     valor = models.IntegerField(default=0)
-    encabezado = models.ForeignKey(EncabTablaR49, on_delete=models.CASCADE)
+    encabezado = models.ForeignKey(EncabTablaR49, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         pass
