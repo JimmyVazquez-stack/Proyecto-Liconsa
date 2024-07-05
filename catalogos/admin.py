366341d6606d18875b32cc002af552d529ed8a35
Jucamora <166667024+Jucamora@users.noreply.github.com>
2024-07-05 14:45:25 -0600

    Agregar modelo Encabezado tabla r49 al admin

--- catalogos/admin.py
+++ catalogos/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Lecheria, Ruta, Poblacion, Rotos
-from laboratorio_control_calidad.models import Densidadpt, Pesoenvvacio, Pesobruto #Pesoneto
+from laboratorio_control_calidad.models import Densidadpt, Pesoenvvacio, Pesobruto, EncabTablaR49 #Pesoneto
 from .models import Lecheria, Ruta, Poblacion, Rotos, Area, Producto, Planta, Proveedor, Maquina, Cabezal, Turno, Silo, TipoProducto, Analista
 
 # Register your models here.
@@ -13,6 +13,8 @@ admin.site.register(Rotos)
 admin.site.register(Densidadpt)
 admin.site.register(Pesoenvvacio)
 admin.site.register(Pesobruto)
+admin.site.register(EncabTablaR49)
+
 # admin.site.register(Pesoneto)
 admin.site.register(Area)
 admin.site.register(Producto)
