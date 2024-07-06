29d2125ac04289c41d9daec0dba765ff0f347b3b
Eduardo <calle13.201@gmail.com>
2024-07-06 17:19:14 -0600

    Turno admin para campo TimeInput

--- catalogos/admin.py
+++ catalogos/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Lecheria, Ruta, Poblacion, Rotos, Area, Producto, Planta, Proveedor, Maquina, Cabezal, Turno, Silo, TipoProducto
+from django.forms import TimeInput
+from .forms import TurnoForm
+from django.db import models
 
 # Register your models here.
 admin.site.register(Lecheria)
@@ -12,6 +15,17 @@ admin.site.register(Planta)
 admin.site.register(Proveedor)
 admin.site.register(Maquina)
 admin.site.register(Cabezal)
-admin.site.register(Turno)
 admin.site.register(Silo)
 admin.site.register(TipoProducto)
+
+class TurnoAdmin(admin.ModelAdmin):
+    form= TurnoForm
+    formfield_overrides = {
+        models.TimeField: {'widget': TimeInput(attrs={'type': 'time'})},
+    }
+    
+admin.site.register(Turno, TurnoAdmin)
+
+'''
+Turno admin con TimeInput para que se muestre el campo de tiempo en el admin
+'''
