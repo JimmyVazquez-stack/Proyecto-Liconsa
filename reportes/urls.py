6dfae1ca466ac1e4bf1387cae53d56b270b44cdb
Juan Carlos Morales <juancarlos.morales@outlook.com>
2024-07-17 19:44:28 -0600

    url para reporte, en app reportes

--- reportes/urls.py
+++ reportes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ReporteMensualView
+from .views import *
 
 app_name = 'reportes'   
 
 urlpatterns = [
-    path('reporte-mensual/', ReporteMensualView.as_view(), name='reporte_mensual')
+    path('reporte-mensual/', ReporteMensualView.as_view(), name='reporte_mensual'),
+    path('reporte-volumen-neto/', VolumenNetoView.as_view(), name='reporte_Volumen_Neto'),
 ]
