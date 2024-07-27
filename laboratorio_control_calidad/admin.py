91f87a98de30eb5490b13e66b2cda7398ed842ab
Jucamora <166667024+Jucamora@users.noreply.github.com>
2024-07-26 20:30:35 -0600

    Mover modelos del admin catalogos al admin laboratorio.

--- laboratorio_control_calidad/admin.py
+++ laboratorio_control_calidad/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+from .models import Densidadpt, Pesoenvvacio, Pesobruto, EncabTablaR49
 
 # Register your models here.
-
+#Modelos FormatoR49
+admin.site.register(Densidadpt)
+admin.site.register(Pesoenvvacio)
+admin.site.register(Pesobruto)
+admin.site.register(EncabTablaR49)
