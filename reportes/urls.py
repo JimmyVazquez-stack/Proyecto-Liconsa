bcb89c923c16deae63c1af9f7d6e9053112489fa
Jucamora <166667024+Jucamora@users.noreply.github.com>
2024-07-26 20:25:46 -0600

    Agregar url para data de calculos con json en la vista

--- reportes/urls.py
+++ reportes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import *
+from reportes.views import CalculosR49DataView, VolumenNetoView, ReporteMensualView
 
 app_name = 'reportes'   
 
 urlpatterns = [
     path('reporte-mensual/', ReporteMensualView.as_view(), name='reporte_mensual'),
     path('reporte-volumen-neto/', VolumenNetoView.as_view(), name='reporte_Volumen_Neto'),
+    path('densidad-Ponderada-Data/', CalculosR49DataView.as_view(), name='densidad_Ponderada_Data'),
 ]
