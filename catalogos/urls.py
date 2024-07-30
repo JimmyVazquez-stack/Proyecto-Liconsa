c3ecd1a94659573324eef8922a8bcf003fa310fc
Eduardo <calle13.201@gmail.com>
2024-07-29 23:45:58 -0600

    urls para rutas de catalogos

--- catalogos/urls.py
+++ catalogos/urls.py
@@ -14,6 +14,21 @@ from .views import (
     SiloListView,
     TurnoListView,
     DataPoblacionView,
+    PlantaDataView,
+    TurnoDataView,
+    DataSilosView,
+    DataAreaView,
+    CabezalDataView,
+    ProductoDataView,
+    ProductoListView,
+    TipoProductoDataView,
+    TipoProductoListView,
+    ProveedorDataView,
+    RutaListView,
+    RutaDataView,
+    RutaCreateView,
+    RutaUpdateView,
+    RutaDeleteView
     )
 
 app_name = 'catalogos'
@@ -28,17 +43,34 @@ urlpatterns = [
     path('poblaciones/list/data/', DataPoblacionView.as_view(), name='poblaciones_data'),
     #rutas a áreas
     path('areas/list/', AreaListView.as_view(), name='areas_list'),
+    path('areas/list/data/', DataAreaView.as_view(), name='areas_data'),
     #rutas a maquinas
     path('maquinas/list/', MaquinaListView.as_view(), name='maquinas_list'),
     path('maquinas/list/data/', MaquinaDataView.as_view(), name='maquinas_data'),
     #rutas a cabezales
     path('cabezales/list/', CabezalListView.as_view(), name='cabezales_list'),
+    path('cabezales/list/data/', CabezalDataView.as_view(), name='cabezales_data'),
     #rutas a plantas
     path('plantas/list/', PlantaListView.as_view(), name='plantas_list'),
+    path('plantas/list/data/', PlantaDataView.as_view(), name='plantas_data'),
     #rutas a proveedores
     path('proveedores/list/', ProveedorListView.as_view(), name='proveedores_list'),
+    path('proveedores/list/data/', ProveedorDataView.as_view(), name='proveedores_data'),
     #rutas a turnos
     path('turnos/list/', TurnoListView.as_view(), name='turnos_list'),
+    path('turnos/list/data/', TurnoDataView.as_view(), name='turnos_data'),
     #rutas a silos
     path('silos/list/', SiloListView.as_view(), name='silos_list'),
+    path('silos/list/data/', DataSilosView.as_view(), name='silos_data'),
+    #rutas a gestion de productos
+    path('productos/list/', ProductoListView.as_view(), name='productos_list'),
+    path('productos/list/data/', ProductoDataView.as_view(), name='productos_data'),
+    path('tipo_producto/list/', TipoProductoListView.as_view(), name='tipo_producto_list'),
+    path('tipo_producto/list/data/', TipoProductoDataView.as_view(), name='tipo_producto_data'),
+    #rutas
+    path('rutas/list/', RutaListView.as_view(), name='rutas_list'),
+    path('rutas/list/data/', RutaDataView.as_view(), name='rutas_data'),
+    path('rutas/create/', RutaCreateView.as_view(), name='rutas_create'),
+    path('rutas/update/<int:pk>/', RutaUpdateView.as_view(), name='rutas_update'),
+    path('rutas/delete/<int:id>/', RutaDeleteView.as_view(), name='rutas_delete'),
     ]
