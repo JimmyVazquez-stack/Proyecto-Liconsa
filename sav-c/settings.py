512b8a0b65cc9411f5ef9f5409293a5815b71b23
Jucamora <166667024+Jucamora@users.noreply.github.com>
2024-07-31 18:54:47 -0600

    Agregar el context global de usuarios al settings

--- sav-c/settings.py
+++ sav-c/settings.py
@@ -135,6 +135,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'usuarios.context_processors.user_context',  #para context global de usuario y grupo
             ],
         },
     },
