618577de3e5aea33c1d2bb3965f96c5e49c3f071
Jimmy <jv78676@gmail.com>
2024-08-07 23:46:47 -0600

    Hice unos pequeños cambios y ademas cree una vista para mi apartado que es el reporte mensual

--- reportes/urls.py
+++ reportes/urls.py
@@ -3,7 +3,11 @@ from . import views
 app_name = 'reportes'   
 
 urlpatterns = [
-    path('reporte-mensual/', views.PDFGeneratorView.as_view(), name='reporte_mensual'),
     path('reporte-Rx50/<int:pk>', views.ReporteRX50.as_view(), name='reporte_Rx50'),
-    path('reporte-Rx51/<int:pk>', views.ReporteRX51.as_view(), name='reporte_Rx51')
+    path('reporte-Rx51/<int:pk>', views.ReporteRX51.as_view(), name='reporte_Rx51'),
+
+    #URLS para reporte mensual
+    path('reporte-mensual-lab/', views.ReporteMensual.as_view(), name='reporte_mensual'),
+    path('reporte-mensual-lab/pdf/', views.PDFGeneratorView.as_view(), name='reporte_mensual_pdf'),
+
 ]
