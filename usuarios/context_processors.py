6d89defe0edcf1e2b5b2548838075f75ec7d87d7
Jucamora <166667024+Jucamora@users.noreply.github.com>
2024-08-09 15:02:42 -0600

    Merge  con Eduardo

--- usuarios/context_processors.py
+++ usuarios/context_processors.py
@@ -19,15 +19,15 @@ def user_context(request):
     }
 
 
-# def user_group_processor(request):
-#     if request.user.is_authenticated:
-#         es_coordinador = request.user.groups.filter(name__iexact='coordinador').exists()
-#         es_staff = request.user.is_staff
-#     else:
-#         es_coordinador = False
-#         es_staff = False
+def user_group_processor(request):
+    if request.user.is_authenticated:
+        es_coordinador = request.user.groups.filter(name__iexact='coordinador').exists()
+        es_staff = request.user.is_staff
+    else:
+        es_coordinador = False
+        es_staff = False
 
-#     return {
-#         'es_coordinador': es_coordinador,
-#         'es_staff': es_staff,
-#     }
+    return {
+        'es_coordinador': es_coordinador,
+        'es_staff': es_staff,
+    }
