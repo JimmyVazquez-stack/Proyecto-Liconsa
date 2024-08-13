6e5f70f097ce6571977ccbbb1b11772ccc071cb6
Eduardo <calle13.201@gmail.com>
2024-08-12 21:04:27 -0600

    Ultimos cambios migraciones

--- reportes/urls.py
+++ reportes/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import ReporteMensualView, ReporteRX50, ReporteRX51
+from .views import ReporteMensual, ReporteRX50, ReporteRX51
 
 app_name = 'reportes'   
 
 urlpatterns = [
-    path('reporte-mensual/', ReporteMensualView.as_view(), name='reporte_mensual'),
+    path('reporte-mensual/', ReporteMensual.as_view(), name='reporte_mensual'),
     path('reporte-Rx50/<int:pk>', ReporteRX50.as_view(), name='reporte_Rx50'),
     #reporte rx51
-    path('reporte-mensual/', ReporteMensualView.as_view(), name='reporte_mensual'),
+    path('reporte-mensual/', ReporteMensual.as_view(), name='reporte_mensual'),
     path('reporte-Rx51/<int:pk>', ReporteRX51.as_view(), name='reporte_Rx51')
     
     
