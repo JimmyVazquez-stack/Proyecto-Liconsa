dfae3424716dd19271e1a77acfb3664b0f6f1ab2
Jucamora <166667024+Jucamora@users.noreply.github.com>
2024-08-13 01:47:08 -0600

    Crear archivo para librerias externas

---
+++ laboratorio_control_calidad/static/js/datatables/external-libraries.js
@@ -0,0 +1,9 @@
+// static/js/external-libraries.js
+
+// Librerías externas necesarias para DataTables
+// Incluye jQuery, Bootstrap, y DataTables
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap4.min.js"></script>
