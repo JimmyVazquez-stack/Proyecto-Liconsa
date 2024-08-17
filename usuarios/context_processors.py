a223041e02b36d8e6e1caf8afe9cce88b7fbe4d0
Jucamora <166667024+Jucamora@users.noreply.github.com>
2024-08-16 19:21:09 -0600

    Fusion processors de Lalo y mio, daba conflicto por separado.

--- usuarios/context_processors.py
+++ usuarios/context_processors.py
@@ -2,32 +2,22 @@
 
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
-
 def user_context(request):
+    user = request.user
     user_info = None
     user_groups = None
-    if request.user.is_authenticated:
-        try:
-            user_info = User.objects.get(username=request.user.username)
-            user_groups = user_info.groups.all()
-        except User.DoesNotExist:
-            pass
+    es_coordinador = False
+    es_staff = False
+
+    if user.is_authenticated:
+        user_info = user
+        user_groups = user.groups.all()
+        es_coordinador = user.groups.filter(name__iexact='coordinador').exists()
+        es_staff = user.is_staff
+
     return {
         'user_info': user_info,
-          'user_groups': user_groups,
-    }
-
-
-def user_group_processor(request):
-    if request.user.is_authenticated:
-        es_coordinador = request.user.groups.filter(name__iexact='coordinador').exists()
-        es_staff = request.user.is_staff
-    else:
-        es_coordinador = False
-        es_staff = False
-
-    return {
+        'user_groups': user_groups,
         'es_coordinador': es_coordinador,
         'es_staff': es_staff,
     }
