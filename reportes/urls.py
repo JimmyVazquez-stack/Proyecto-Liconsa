c28ef6adad35411814b1c3737c65b72a2e8ae42d
Jucamora <166667024+Jucamora@users.noreply.github.com>
2024-08-26 12:56:16 -0600

    url para vista datos json diario-semanal

--- reportes/urls.py
+++ reportes/urls.py
@@ -7,8 +7,12 @@ app_name = 'reportes'
 urlpatterns = [
     path('reporte-Rx50/<int:pk>', views.ReporteRX50.as_view(), name='reporte_Rx50'),
     path('reporte-Rx51/<int:pk>', views.ReporteRX51.as_view(), name='reporte_Rx51'),
+
+    #URLS para reporte R49 JuanCarlosM
     path('reporte-volumen-neto/', views.VolumenNetoView.as_view(), name='reporte_Volumen_Neto'),
     path('densidad-Ponderada-Data/', views.CalculosR49DataView.as_view(), name='densidad_Ponderada_Data'),
+    path('diario-Semanal-Data/', views.ReporteR49RangoFechaView.as_view(), name='diario_Semanal_Data'),
+    path('reporte-Diario-Semanal/', views.MostrarDiarioSemanalView.as_view(), name='reporte_Diario_Semanal'),
     #URLS para reporte mensual
     path('reporte-mensual-lab/', views.ReporteMensualView.as_view(), name='reporte_mensual'),
     path('reporte-mensual-lab/pdf/', views.PDFGeneratorView.as_view(), name='reporte_mensual_pdf'),
