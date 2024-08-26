98ae866dcec7a51c04ab0bbf2c9862a68623dc95
Jucamora <166667024+Jucamora@users.noreply.github.com>
2024-08-26 13:07:51 -0600

    Agregar al archivo script de static el codigo para el encab list del datatable

--- laboratorio_control_calidad/static/js/scripts.js
+++ laboratorio_control_calidad/static/js/scripts.js
@@ -5,3 +5,25 @@ document.addEventListener('DOMContentLoaded', function() {
         alert('Por favor corrige los errores en el formulario.');
     }
 });
+
+
+//  Page specific script datatable 
+$(function() {
+    $('.btnTest').on('click', function() {
+        $.ajax({
+            url: "{% url 'laboratorio_control_calidad:encabezador49V2_List'%}" , 
+            type: 'POST',
+            data: {
+                id: 1
+            },
+            dataType: 'json'
+        }).done(function(data) {
+            console.log(data)
+        }).fail(function(data) {
+            alert("error");
+        }).always(function(data) {
+
+        })
+    });
+
+});
