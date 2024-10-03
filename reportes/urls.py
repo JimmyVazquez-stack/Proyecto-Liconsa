f69e0d865e9916b6bfa1c67cc1dea91ecadbfe51
Jucamora <166667024+Jucamora@users.noreply.github.com>
2024-10-02 19:30:26 -0600

    correccion urls

--- reportes/urls.py
+++ reportes/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
 
     #URLS para reporte R49 JuanCarlosM
     path('reporte-volumen-neto/', views.VolumenNetoView.as_view(), name='reporte_Volumen_Neto'),
-    path('densidad-Ponderada-Data/', views.CalculosR49DataView.as_view(), name='densidad_Ponderada_Data'),
-    path('diario-Semanal-Data/', views.ReporteR49RangoFechaView.as_view(), name='diario_Semanal_Data'),
-    path('reporte-Diario-Semanal/', views.MostrarDiarioSemanalView.as_view(), name='reporte_Diario_Semanal'),
+    path('densidad-Ponderada-Data/', views.CalculosR49DataView.as_view(), name='densidad_Ponderada_Data'), #calculos para tabla peso neto
+    path('diario-Semanal-Data/', views.ReporteR49RangoFechaView.as_view(), name='diario_Semanal_Data'), #no se muestran los datos directo en la url, pero si desde el modal con rango de fecha
+    path('reporte-Diario/<int:id>', views.MostrarDiarioView.as_view(), name='reporte_Diario'), #Para reporte diario por maquinas, peso neto
     #URLS para reporte mensual
     path('reporte-mensual-lab/', views.ReporteMensualView.as_view(), name='reporte_mensual'),
     path('reporte-mensual-lab/pdf/', views.PDFGeneratorView.as_view(), name='reporte_mensual_pdf'),
