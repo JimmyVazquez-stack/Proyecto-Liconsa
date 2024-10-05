b97d0f094544d3e815e5599097b35957290c9e1b
Jucamora <166667024+Jucamora@users.noreply.github.com>
2024-10-05 15:02:58 -0600

    Agregar botones a datatables R49, actualizacion de archivo js

--- laboratorio_control_calidad/static/js/datatables/datatables-config.js
+++ laboratorio_control_calidad/static/js/datatables/datatables-config.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
     // Configurar DataTables para cada tabla
-    $('#tablaPesoBruto').DataTable({
+    var tablaPesoBruto = $('#tablaPesoBruto').DataTable({
         "pageLength": 10, // Mostrar 10 registros por página
         "responsive": true, // Hacer la tabla responsiva
         "lengthChange": true, // Mostrar opciones de cantidad de registros
@@ -10,13 +10,53 @@ $(document).ready(function() {
         "ordering": true, // Habilitar la ordenación de columnas
         "info": true, // Mostrar la información sobre los registros
         "autoWidth": false, // Deshabilitar ajuste automático de ancho
+        "dom": 'Bfrtip', // Integrar botones con la tabla
+        "buttons":{
+            "buttons":[
+                {
+                    extend:'copy',
+                    text:'<i class="bi bi-clipboard-fill"></i>',
+                    tittleAttr:'Copiar',
+                    className:'btn btn-secondary'
+                },
+                {
+                    extend:'excel',
+                    text:'<i class="bi bi-file-earmark-spreadsheet"></i>',
+                    tittleAttr:'Exportar a excel',
+                    className:'btn btn-success'
+                },
+                {
+                    extend:'pdf',
+                    text:'<i class="bi bi-file-earmark-pdf"></i>',
+                    tittleAttr:'Exportar a pdf',
+                    className:'btn btn-secondary'
+                },
+                {
+                    extend:'print',
+                    text:'<i class="bi bi-printer"></i>',
+                    tittleAttr:'Imprimir',
+                    className:'btn btn-secondary'
+                },
+                {
+                    extend:'colvis',
+                    text:'Filtrar columnas'
+                },
+                
+
+            ]
+        },
+        /*"buttons": [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ],*/
         "language": {
             "url": "//cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json" // Traducción al español (opcional)
         }
     });
+    // Inicializar los botones
+    tablaPesoBruto.buttons().container().appendTo('#tablaPesoBruto_wrapper .col-md-6:eq(0)');
 
     // Repetir la configuración para las otras tablas
-    $('#tablaDensidad').DataTable({
+    var tablaDensidad = $('#tablaDensidad').DataTable({
         "pageLength": 10,
         "responsive": true,
         "lengthChange": true,
@@ -24,12 +64,18 @@ $(document).ready(function() {
         "ordering": true,
         "info": true,
         "autoWidth": false,
+        "dom": 'Bfrtip', // Integrar botones con la tabla
+        "buttons": [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ],
         "language": {
             "url": "//cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json"
         }
     });
+    // Inicializar los botones
+    tablaDensidad.buttons().container().appendTo('#tablaDensidad_wrapper .col-md-6:eq(0)');
 
-    $('#tablaPesoEnvVacio').DataTable({
+    var tablaPesoEnvVacio = $('#tablaPesoEnvVacio').DataTable({
         "pageLength": 10,
         "responsive": true,
         "lengthChange": true,
@@ -37,8 +83,14 @@ $(document).ready(function() {
         "ordering": true,
         "info": true,
         "autoWidth": false,
+        "dom": 'Bfrtip', // Integrar botones con la tabla
+        "buttons": [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ],
         "language": {
             "url": "//cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json"
         }
     });
+    // Inicializar los botones
+    tablaPesoEnvVacio.buttons().container().appendTo('#tablaPesoEnvVacio_wrapper .col-md-6:eq(0)');
 });
