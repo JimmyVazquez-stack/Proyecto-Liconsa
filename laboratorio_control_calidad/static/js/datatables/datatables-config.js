6b81c6fcbf1f3e4846a99e21b124a36ba8d67bf6
Juan Carlos Morales <juancarlos.morales@outlook.com>
2024-10-06 22:50:31 -0600

    Cambios en archivo js para botones datatables

--- laboratorio_control_calidad/static/js/datatables/datatables-config.js
+++ laboratorio_control_calidad/static/js/datatables/datatables-config.js
@@ -1,57 +1,66 @@
 // static/js/datatables-config.js
 
 $(document).ready(function() {
+
+    // Función para obtener el título basado en la pestaña activa
+    function getActiveTabTitle() {
+        // Obtiene la pestaña activa
+        var activeTab = $('.nav-tabs .nav-link.active').text();
+        // Retorna el título basado en la pestaña activa
+        return activeTab.trim(); // Limpia espacios en blanco
+    }
+
     // Configurar DataTables para cada tabla
     var tablaPesoBruto = $('#tablaPesoBruto').DataTable({
-        "pageLength": 10, // Mostrar 10 registros por página
-        "responsive": true, // Hacer la tabla responsiva
-        "lengthChange": true, // Mostrar opciones de cantidad de registros
-        "searching": true, // Habilitar la búsqueda
-        "ordering": true, // Habilitar la ordenación de columnas
-        "info": true, // Mostrar la información sobre los registros
-        "autoWidth": false, // Deshabilitar ajuste automático de ancho
-        "dom": 'Bfrtip', // Integrar botones con la tabla
-        "buttons":{
-            "buttons":[
-                {
-                    extend:'copy',
-                    text:'<i class="bi bi-clipboard-fill"></i>',
-                    tittleAttr:'Copiar',
-                    className:'btn btn-secondary'
-                },
-                {
-                    extend:'excel',
-                    text:'<i class="bi bi-file-earmark-spreadsheet"></i>',
-                    tittleAttr:'Exportar a excel',
-                    className:'btn btn-success'
-                },
-                {
-                    extend:'pdf',
-                    text:'<i class="bi bi-file-earmark-pdf"></i>',
-                    tittleAttr:'Exportar a pdf',
-                    className:'btn btn-secondary'
-                },
-                {
-                    extend:'print',
-                    text:'<i class="bi bi-printer"></i>',
-                    tittleAttr:'Imprimir',
-                    className:'btn btn-secondary'
-                },
-                {
-                    extend:'colvis',
-                    text:'Filtrar columnas'
-                },
-                
-
-            ]
-        },
-        /*"buttons": [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ],*/
-        "language": {
-            "url": "//cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json" // Traducción al español (opcional)
-        }
+        "pageLength": 10,
+        "responsive": true,
+        "lengthChange": true,
+        "searching": true,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+        "dom": 'Bfrtip',
+        "buttons": [
+            {
+                extend: 'copy',
+                text: '<i class="bi bi-clipboard-fill"></i>',
+                titleAttr: 'Copiar',
+                className: 'btn btn-secondary',
+                title: function() { return getActiveTabTitle(); } // Título basado en la pestaña activa
+            },
+            {
+                extend: 'excel',
+                text: '<i class="bi bi-file-earmark-spreadsheet"></i>',
+                titleAttr: 'Exportar a excel',
+                className: 'btn btn-success',
+                title: function() { return getActiveTabTitle(); }, // Título basado en la pestaña activa
+                exportOptions: {
+                    columns: ':not(:last-child)' // Excluir la última columna
+                }
+            },
+            {
+                extend: 'pdf',
+                text: '<i class="bi bi-file-earmark-pdf"></i>',
+                titleAttr: 'Exportar a pdf',
+                className: 'btn btn-danger',
+                title: function() { return getActiveTabTitle(); }, // Título basado en la pestaña activa
+                exportOptions: {
+                    columns: ':not(:last-child)' // Excluir la última columna
+                }
+            },
+            {
+                extend: 'print',
+                text: '<i class="bi bi-printer"></i>',
+                titleAttr: 'Imprimir',
+                className: 'btn btn-secondary',
+                title: function() { return getActiveTabTitle(); }, // Título basado en la pestaña activa
+                exportOptions: {
+                    columns: ':not(:last-child)' // Excluir la última columna
+                }
+            },
+        ]
     });
+
     // Inicializar los botones
     tablaPesoBruto.buttons().container().appendTo('#tablaPesoBruto_wrapper .col-md-6:eq(0)');
 
@@ -64,14 +73,48 @@ $(document).ready(function() {
         "ordering": true,
         "info": true,
         "autoWidth": false,
-        "dom": 'Bfrtip', // Integrar botones con la tabla
+        "dom": 'Bfrtip',
         "buttons": [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ],
-        "language": {
-            "url": "//cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json"
-        }
+            {
+                extend: 'copy',
+                text: '<i class="bi bi-clipboard-fill"></i>',
+                titleAttr: 'Copiar',
+                className: 'btn btn-secondary',
+                title: function() { return getActiveTabTitle(); } // Título basado en la pestaña activa
+            },
+            {
+                extend: 'excel',
+                text: '<i class="bi bi-file-earmark-spreadsheet"></i>',
+                titleAttr: 'Exportar a excel',
+                className: 'btn btn-success',
+                title: function() { return getActiveTabTitle(); }, // Título basado en la pestaña activa
+                exportOptions: {
+                    columns: ':not(:last-child)'
+                }
+            },
+            {
+                extend: 'pdf',
+                text: '<i class="bi bi-file-earmark-pdf"></i>',
+                titleAttr: 'Exportar a pdf',
+                className: 'btn btn-danger',
+                title: function() { return getActiveTabTitle(); }, // Título basado en la pestaña activa
+                exportOptions: {
+                    columns: ':not(:last-child)'
+                }
+            },
+            {
+                extend: 'print',
+                text: '<i class="bi bi-printer"></i>',
+                titleAttr: 'Imprimir',
+                className: 'btn btn-secondary',
+                title: function() { return getActiveTabTitle(); }, // Título basado en la pestaña activa
+                exportOptions: {
+                    columns: ':not(:last-child)'
+                }
+            },
+        ]
     });
+
     // Inicializar los botones
     tablaDensidad.buttons().container().appendTo('#tablaDensidad_wrapper .col-md-6:eq(0)');
 
@@ -83,14 +126,48 @@ $(document).ready(function() {
         "ordering": true,
         "info": true,
         "autoWidth": false,
-        "dom": 'Bfrtip', // Integrar botones con la tabla
+        "dom": 'Bfrtip',
         "buttons": [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ],
-        "language": {
-            "url": "//cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json"
-        }
+            {
+                extend: 'copy',
+                text: '<i class="bi bi-clipboard-fill"></i>',
+                titleAttr: 'Copiar',
+                className: 'btn btn-secondary',
+                title: function() { return getActiveTabTitle(); } // Título basado en la pestaña activa
+            },
+            {
+                extend: 'excel',
+                text: '<i class="bi bi-file-earmark-spreadsheet"></i>',
+                titleAttr: 'Exportar a excel',
+                className: 'btn btn-success',
+                title: function() { return getActiveTabTitle(); }, // Título basado en la pestaña activa
+                exportOptions: {
+                    columns: ':not(:last-child)'
+                }
+            },
+            {
+                extend: 'pdf',
+                text: '<i class="bi bi-file-earmark-pdf"></i>',
+                titleAttr: 'Exportar a pdf',
+                className: 'btn btn-danger',
+                title: function() { return getActiveTabTitle(); }, // Título basado en la pestaña activa
+                exportOptions: {
+                    columns: ':not(:last-child)'
+                }
+            },
+            {
+                extend: 'print',
+                text: '<i class="bi bi-printer"></i>',
+                titleAttr: 'Imprimir',
+                className: 'btn btn-secondary',
+                title: function() { return getActiveTabTitle(); }, // Título basado en la pestaña activa
+                exportOptions: {
+                    columns: ':not(:last-child)'
+                }
+            },
+        ]
     });
+
     // Inicializar los botones
     tablaPesoEnvVacio.buttons().container().appendTo('#tablaPesoEnvVacio_wrapper .col-md-6:eq(0)');
 });
