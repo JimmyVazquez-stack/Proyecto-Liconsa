37069339360a10cc5e3620fa684327d2107ec1da
Eduardo <calle13.201@gmail.com>
2025-01-07 14:52:35 -0600

    modificaciones

--- usuarios/context_processors.py
+++ usuarios/context_processors.py
@@ -1,12 +1,15 @@
 def user_group_processor(request):
     if request.user.is_authenticated:
         es_coordinador = request.user.groups.filter(name__iexact='coordinador').exists()
+        es_jefe_unidad = request.user.groups.filter(name__iexact='jefe de unidad').exists()  # Nuevo
         es_staff = request.user.is_staff
     else:
         es_coordinador = False
+        es_jefe_unidad = False
         es_staff = False
 
     return {
         'es_coordinador': es_coordinador,
+        'es_jefe_unidad': es_jefe_unidad,  # Añadido al contexto
         'es_staff': es_staff,
     }
